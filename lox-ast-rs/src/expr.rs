b915e68c1fd7f55f368b34f7d05f6b078a3c1605
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-06-03 11:54:38 -0300

    fix generate ast

---
+++ lox-ast-rs/src/expr.rs
@@ -0,0 +1,157 @@
+use crate::error::*;
+use crate::token::*;
+use crate::object::*;
+use std::rc::Rc;
+
+pub enum Expr {
+    Assign(Rc<AssignExpr>),
+    Binary(Rc<BinaryExpr>),
+    Call(Rc<CallExpr>),
+    Get(Rc<GetExpr>),
+    Grouping(Rc<GroupingExpr>),
+    Literal(Rc<LiteralExpr>),
+    Logical(Rc<LogicalExpr>),
+    Set(Rc<SetExpr>),
+    Super(Rc<SuperExpr>),
+    This(Rc<ThisExpr>),
+    Unary(Rc<UnaryExpr>),
+    Variable(Rc<VariableExpr>),
+}
+
+impl PartialEq for Expr {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Expr::Assign(a), Expr::Assign(b)) => Rc::ptr_eq(a, b),
+            (Expr::Binary(a), Expr::Binary(b)) => Rc::ptr_eq(a, b),
+            (Expr::Call(a), Expr::Call(b)) => Rc::ptr_eq(a, b),
+            (Expr::Get(a), Expr::Get(b)) => Rc::ptr_eq(a, b),
+            (Expr::Grouping(a), Expr::Grouping(b)) => Rc::ptr_eq(a, b),
+            (Expr::Literal(a), Expr::Literal(b)) => Rc::ptr_eq(a, b),
+            (Expr::Logical(a), Expr::Logical(b)) => Rc::ptr_eq(a, b),
+            (Expr::Set(a), Expr::Set(b)) => Rc::ptr_eq(a, b),
+            (Expr::Super(a), Expr::Super(b)) => Rc::ptr_eq(a, b),
+            (Expr::This(a), Expr::This(b)) => Rc::ptr_eq(a, b),
+            (Expr::Unary(a), Expr::Unary(b)) => Rc::ptr_eq(a, b),
+            (Expr::Variable(a), Expr::Variable(b)) => Rc::ptr_eq(a, b),
+            _ => false,
+        }
+    }
+}
+
+impl Eq for Expr{}
+
+use std::hash::{Hash, Hasher};
+impl Hash for Expr {
+    fn hash<H>(&self, hasher: &mut H)
+    where H: Hasher,
+    { match self { 
+        Expr::Assign(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Binary(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Call(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Get(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Grouping(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Literal(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Logical(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Set(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Super(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::This(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Unary(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Expr::Variable(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        }
+    }
+}
+
+impl Expr {
+    pub fn accept<T>(&self, wrapper: Rc<Expr>, expr_visitor: &dyn ExprVisitor<T>) -> Result<T, LoxResult> {
+        match self {
+            Expr::Assign(v) => expr_visitor.visit_assign_expr(wrapper, v),
+            Expr::Binary(v) => expr_visitor.visit_binary_expr(wrapper, v),
+            Expr::Call(v) => expr_visitor.visit_call_expr(wrapper, v),
+            Expr::Get(v) => expr_visitor.visit_get_expr(wrapper, v),
+            Expr::Grouping(v) => expr_visitor.visit_grouping_expr(wrapper, v),
+            Expr::Literal(v) => expr_visitor.visit_literal_expr(wrapper, v),
+            Expr::Logical(v) => expr_visitor.visit_logical_expr(wrapper, v),
+            Expr::Set(v) => expr_visitor.visit_set_expr(wrapper, v),
+            Expr::Super(v) => expr_visitor.visit_super_expr(wrapper, v),
+            Expr::This(v) => expr_visitor.visit_this_expr(wrapper, v),
+            Expr::Unary(v) => expr_visitor.visit_unary_expr(wrapper, v),
+            Expr::Variable(v) => expr_visitor.visit_variable_expr(wrapper, v),
+        }
+    }
+}
+
+pub struct AssignExpr {
+    pub name: Token,
+    pub value: Rc<Expr>,
+}
+
+pub struct BinaryExpr {
+    pub left: Rc<Expr>,
+    pub operator: Token,
+    pub right: Rc<Expr>,
+}
+
+pub struct CallExpr {
+    pub callee: Rc<Expr>,
+    pub paren: Token,
+    pub arguments: Vec<Rc<Expr>>,
+}
+
+pub struct GetExpr {
+    pub object: Rc<Expr>,
+    pub name: Token,
+}
+
+pub struct GroupingExpr {
+    pub expression: Rc<Expr>,
+}
+
+pub struct LiteralExpr {
+    pub value: Option<Object>,
+}
+
+pub struct LogicalExpr {
+    pub left: Rc<Expr>,
+    pub operator: Token,
+    pub right: Rc<Expr>,
+}
+
+pub struct SetExpr {
+    pub object: Rc<Expr>,
+    pub name: Token,
+    pub value: Rc<Expr>,
+}
+
+pub struct SuperExpr {
+    pub keyword: Token,
+    pub method: Token,
+}
+
+pub struct ThisExpr {
+    pub keyword: Token,
+}
+
+pub struct UnaryExpr {
+    pub operator: Token,
+    pub right: Rc<Expr>,
+}
+
+pub struct VariableExpr {
+    pub name: Token,
+}
+
+pub trait ExprVisitor<T> {
+    fn visit_assign_expr(&self, wrapper: Rc<Expr>, expr: &AssignExpr) -> Result<T, LoxResult>;
+    fn visit_binary_expr(&self, wrapper: Rc<Expr>, expr: &BinaryExpr) -> Result<T, LoxResult>;
+    fn visit_call_expr(&self, wrapper: Rc<Expr>, expr: &CallExpr) -> Result<T, LoxResult>;
+    fn visit_get_expr(&self, wrapper: Rc<Expr>, expr: &GetExpr) -> Result<T, LoxResult>;
+    fn visit_grouping_expr(&self, wrapper: Rc<Expr>, expr: &GroupingExpr) -> Result<T, LoxResult>;
+    fn visit_literal_expr(&self, wrapper: Rc<Expr>, expr: &LiteralExpr) -> Result<T, LoxResult>;
+    fn visit_logical_expr(&self, wrapper: Rc<Expr>, expr: &LogicalExpr) -> Result<T, LoxResult>;
+    fn visit_set_expr(&self, wrapper: Rc<Expr>, expr: &SetExpr) -> Result<T, LoxResult>;
+    fn visit_super_expr(&self, wrapper: Rc<Expr>, expr: &SuperExpr) -> Result<T, LoxResult>;
+    fn visit_this_expr(&self, wrapper: Rc<Expr>, expr: &ThisExpr) -> Result<T, LoxResult>;
+    fn visit_unary_expr(&self, wrapper: Rc<Expr>, expr: &UnaryExpr) -> Result<T, LoxResult>;
+    fn visit_variable_expr(&self, wrapper: Rc<Expr>, expr: &VariableExpr) -> Result<T, LoxResult>;
+}
+
---
+++ lox-ast-rs/src/stmt.rs
@@ -0,0 +1,136 @@
+use crate::error::*;
+use crate::expr::*;
+use crate::token::*;
+use std::rc::Rc;
+
+pub enum Stmt {
+    Block(Rc<BlockStmt>),
+    Class(Rc<ClassStmt>),
+    Break(Rc<BreakStmt>),
+    Expression(Rc<ExpressionStmt>),
+    Function(Rc<FunctionStmt>),
+    If(Rc<IfStmt>),
+    Print(Rc<PrintStmt>),
+    Return(Rc<ReturnStmt>),
+    Var(Rc<VarStmt>),
+    While(Rc<WhileStmt>),
+}
+
+impl PartialEq for Stmt {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Stmt::Block(a), Stmt::Block(b)) => Rc::ptr_eq(a, b),
+            (Stmt::Class(a), Stmt::Class(b)) => Rc::ptr_eq(a, b),
+            (Stmt::Break(a), Stmt::Break(b)) => Rc::ptr_eq(a, b),
+            (Stmt::Expression(a), Stmt::Expression(b)) => Rc::ptr_eq(a, b),
+            (Stmt::Function(a), Stmt::Function(b)) => Rc::ptr_eq(a, b),
+            (Stmt::If(a), Stmt::If(b)) => Rc::ptr_eq(a, b),
+            (Stmt::Print(a), Stmt::Print(b)) => Rc::ptr_eq(a, b),
+            (Stmt::Return(a), Stmt::Return(b)) => Rc::ptr_eq(a, b),
+            (Stmt::Var(a), Stmt::Var(b)) => Rc::ptr_eq(a, b),
+            (Stmt::While(a), Stmt::While(b)) => Rc::ptr_eq(a, b),
+            _ => false,
+        }
+    }
+}
+
+impl Eq for Stmt{}
+
+use std::hash::{Hash, Hasher};
+impl Hash for Stmt {
+    fn hash<H>(&self, hasher: &mut H)
+    where H: Hasher,
+    { match self { 
+        Stmt::Block(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::Class(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::Break(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::Expression(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::Function(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::If(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::Print(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::Return(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::Var(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        Stmt::While(a) => { hasher.write_usize(Rc::as_ptr(a) as usize); }
+        }
+    }
+}
+
+impl Stmt {
+    pub fn accept<T>(&self, wrapper: Rc<Stmt>, stmt_visitor: &dyn StmtVisitor<T>) -> Result<T, LoxResult> {
+        match self {
+            Stmt::Block(v) => stmt_visitor.visit_block_stmt(wrapper, v),
+            Stmt::Class(v) => stmt_visitor.visit_class_stmt(wrapper, v),
+            Stmt::Break(v) => stmt_visitor.visit_break_stmt(wrapper, v),
+            Stmt::Expression(v) => stmt_visitor.visit_expression_stmt(wrapper, v),
+            Stmt::Function(v) => stmt_visitor.visit_function_stmt(wrapper, v),
+            Stmt::If(v) => stmt_visitor.visit_if_stmt(wrapper, v),
+            Stmt::Print(v) => stmt_visitor.visit_print_stmt(wrapper, v),
+            Stmt::Return(v) => stmt_visitor.visit_return_stmt(wrapper, v),
+            Stmt::Var(v) => stmt_visitor.visit_var_stmt(wrapper, v),
+            Stmt::While(v) => stmt_visitor.visit_while_stmt(wrapper, v),
+        }
+    }
+}
+
+pub struct BlockStmt {
+    pub statements: Rc<Vec<Rc<Stmt>>>,
+}
+
+pub struct ClassStmt {
+    pub name: Token,
+    pub superclass: Option<Rc<Expr>>,
+    pub methods: Rc<Vec<Rc<Stmt>>>,
+}
+
+pub struct BreakStmt {
+    pub token: Token,
+}
+
+pub struct ExpressionStmt {
+    pub expression: Rc<Expr>,
+}
+
+pub struct FunctionStmt {
+    pub name: Token,
+    pub params: Rc<Vec<Token>>,
+    pub body: Rc<Vec<Rc<Stmt>>>,
+}
+
+pub struct IfStmt {
+    pub condition: Rc<Expr>,
+    pub then_branch: Rc<Stmt>,
+    pub else_branch: Option<Rc<Stmt>>,
+}
+
+pub struct PrintStmt {
+    pub expression: Rc<Expr>,
+}
+
+pub struct ReturnStmt {
+    pub keyword: Token,
+    pub value: Option<Rc<Expr>>,
+}
+
+pub struct VarStmt {
+    pub name: Token,
+    pub initializer: Option<Rc<Expr>>,
+}
+
+pub struct WhileStmt {
+    pub condition: Rc<Expr>,
+    pub body: Rc<Stmt>,
+}
+
+pub trait StmtVisitor<T> {
+    fn visit_block_stmt(&self, wrapper: Rc<Stmt>, stmt: &BlockStmt) -> Result<T, LoxResult>;
+    fn visit_class_stmt(&self, wrapper: Rc<Stmt>, stmt: &ClassStmt) -> Result<T, LoxResult>;
+    fn visit_break_stmt(&self, wrapper: Rc<Stmt>, stmt: &BreakStmt) -> Result<T, LoxResult>;
+    fn visit_expression_stmt(&self, wrapper: Rc<Stmt>, stmt: &ExpressionStmt) -> Result<T, LoxResult>;
+    fn visit_function_stmt(&self, wrapper: Rc<Stmt>, stmt: &FunctionStmt) -> Result<T, LoxResult>;
+    fn visit_if_stmt(&self, wrapper: Rc<Stmt>, stmt: &IfStmt) -> Result<T, LoxResult>;
+    fn visit_print_stmt(&self, wrapper: Rc<Stmt>, stmt: &PrintStmt) -> Result<T, LoxResult>;
+    fn visit_return_stmt(&self, wrapper: Rc<Stmt>, stmt: &ReturnStmt) -> Result<T, LoxResult>;
+    fn visit_var_stmt(&self, wrapper: Rc<Stmt>, stmt: &VarStmt) -> Result<T, LoxResult>;
+    fn visit_while_stmt(&self, wrapper: Rc<Stmt>, stmt: &WhileStmt) -> Result<T, LoxResult>;
+}
+
